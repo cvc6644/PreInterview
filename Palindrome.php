cbdbc55a4a0583de52256673d2ec6d51fcdad1ce
chase.caynoski <chase.caynoski@ccaynoski-pc01.datto.lan>
2018-06-14 10:42:31 -0400

    Start

---
+++ Palindrome.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function palindrome($input){
+    
+    $trimed = strtolower(preg_filter('/\w+/', "", $input));
+    echo $trimed;
+    echo strlen($trimed);
+    //$test = substr_compare($trimed, strrev($trimed), 0, (strlen($trimed)/2));
+    //return $trimed == strrev($trimed);
+    return $test;
+}
---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+include 'Bracketology.php';
+include 'Duplicate.php';
+include 'Palindrome.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo "Problem #1 - Palindrome\n";
+echo palindrome("A man, a plan, a canal, Panama!")."\n";
+echo palindrome("Kayak")."\n";
+echo palindrome("123321")."\n";
+echo palindrome("123421")."\n";
