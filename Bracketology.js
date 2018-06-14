dcdf9f3d69ea34e07d9ac572d5aa36fad50ff4a1
Snowy <Snowy@owlmonster>
2018-06-14 19:50:39 -0400

    Added additional comments to Both Palindrome and Bracketology. Also added an additional fun test to Palindrome

--- Bracketology.js
+++ Bracketology.js
@@ -1,22 +1,22 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 
 function Bracketology(input) {
-    //var result;
+    //eliminates an automatic failure
     if(input.indexOf(")")>0 ){
         var open = 0;
+        /*
+         * Every instance of a ( increments open by 1
+         * Every instance of a ) decrements open by 1
+         */ 
         for ( i=0;i<input.length;i++){
-            if(input.charAt(i)=="("){
+            if(input.charAt(i)==="("){
                 open++;
             }else{
                 open--;
             }
         }
-        result = open ==0;
+        result = (open ===0);
     }else{
         result =false;
     }
