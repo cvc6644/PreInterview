d48f24ae3f8ecb9b3fbac1f99f38fc08f27bf3bc
Snowy <Snowy@owlmonster>
2018-06-14 21:46:49 -0400

    Having an even number of brackets but having more closed brackets before their corresponding open bracket would result in a true. Added check if open goes negative as that would indicate a close bracket without its corresponding open

--- Bracketology.js
+++ Bracketology.js
@@ -1,9 +1,11 @@
 
 
-
+/*this wont work if any non bracket characters are in the string
+ * add 
+ */
 function Bracketology(input) {
-    //eliminates an automatic failure
-    if(input.indexOf(")")>0 ){
+    
+    
         open = 0;
         /*
          * Every instance of a ( increments open by 1
@@ -15,14 +17,18 @@ function Bracketology(input) {
             }else{
                 open--;
             }*/
+            
             input.charAt(i)==="("?open++:open--;
+            //checks if open is less than 0 which would indicate an closed bracket without an open bracket
+            if(open<0){
+                return false;
+            }
         }
         return (open ===0);
-    }else{
-        return false;
-    }
+    
 }
 
 console.log(Bracketology("(()))()"));//false
 console.log(Bracketology("()()(()())"));//true
 console.log(Bracketology(")()("));//false
+console.log(Bracketology("())(()"));
