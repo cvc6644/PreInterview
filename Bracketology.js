84c447f6cba64c9d31346fef547f0f119c0a93b7
Snowy <Snowy@owlmonster>
2018-06-14 21:21:23 -0400

    Changed a simple if else to ternary

--- Bracketology.js
+++ Bracketology.js
@@ -4,23 +4,23 @@
 function Bracketology(input) {
     //eliminates an automatic failure
     if(input.indexOf(")")>0 ){
-        var open = 0;
+        open = 0;
         /*
          * Every instance of a ( increments open by 1
          * Every instance of a ) decrements open by 1
          */ 
         for ( i=0;i<input.length;i++){
-            if(input.charAt(i)==="("){
+            /*if(input.charAt(i)==="("){
                 open++;
             }else{
                 open--;
-            }
+            }*/
+            input.charAt(i)==="("?open++:open--;
         }
-        result = (open ===0);
+        return (open ===0);
     }else{
-        result =false;
+        return false;
     }
-    return result;
 }
 
 console.log(Bracketology("(()))()"));//false
