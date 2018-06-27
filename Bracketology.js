f66466f19b258bb334b1026231dc33b2032011b4
chase.caynoski <chase.caynoski@ccaynoski-pc01.datto.lan>
2018-06-27 12:23:56 -0400

    Updated Bracketology to properly handle only parenthesis Added switch method Added Map method

--- Bracketology.js
+++ Bracketology.js
@@ -1,12 +1,12 @@
 
 
-/*this wont work if any non bracket characters are in the string
- * add 
- */
+//changed so that it properly handles non bracket characters 
 function Bracketology(input) {
     
     
         open = 0;
+        //added map of chars and their intended increment or decrement
+        mapvar = new Map([["(",1],[")",-1]]);
         /*
          * Every instance of a ( increments open by 1
          * Every instance of a ) decrements open by 1
@@ -18,7 +18,26 @@ function Bracketology(input) {
                 open--;
             }*/
             
-            input.charAt(i)==="("?open++:open--;
+            //input.charAt(i)==="("?open++:open--;  original implementaition
+            //
+            //updated implementation for handling only ( or )
+            /*switch (input.charAt(i)){
+                case "(":
+                    open++;
+                    
+                    break;
+                case ")":
+                    open--;
+                    break;
+            }*/
+        
+            /*
+             * it checks if the location
+             * in the map is defined. If it is += it to open. if its not +=0 it to open
+             */
+            open+=mapvar.get(input.charAt(i))?mapvar.get(input.charAt(i)):0;
+            
+            //console.log(open);
             //checks if open is less than 0 which would indicate an closed bracket without an open bracket
             if(open<0){
                 return false;
@@ -29,6 +48,6 @@ function Bracketology(input) {
 }
 
 console.log(Bracketology("(()))()"));//false
-console.log(Bracketology("()()(()())"));//true
-console.log(Bracketology(")()("));//false
-console.log(Bracketology("())(()"));
+console.log(Bracketology("()()fsgsaqwdfwf(()())"));//true
+console.log(Bracketology(")()dawdaw("));//false
+console.log(Bracketology("())wadawfsfrwf(()"));
